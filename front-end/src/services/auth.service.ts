e4c3e949d5e767d9fafa03da3570596d2af8402f
syy7 <felicity_yang@sfu.ca>
2021-10-06 19:52:18 -0700

    department-default

---
+++ front-end/src/services/auth.service.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8088/api/auth/";
+
+class AuthService {
+    login(user) {
+        return axios.post(API_URL + 'signin', {
+            username: user.username,
+            password: user.password
+        }).then(response => {
+            if(response.data.accessToken) {
+                localStorage.setItem('user', JSON.stringify(response.data));
+            }
+            return response.data;
+        });
+    }
+    logout() {
+        localStorage.removeItem('user');
+    }
+    register(user) {
+        return axios.post(API_URL + 'signup', {
+            username: user.username,
+            password: user.password,
+            department: user.department,
+        });
+        
+    }
+}
+
+export default new AuthService();
---
+++ front-end/src/shims-vuex.d.ts
@@ -0,0 +1,16 @@
+import { ComponentCustomProperties } from 'vue'
+// import { Store } from 'vuex'
+import { Store } from '@/store';// path to store file
+
+
+declare module '@vue/runtime-core' {
+  // declare your own store states
+  interface State {
+    count: number
+  }
+
+  // provide typings for `this.$store`
+  interface ComponentCustomProperties {
+    $store: Store
+  }
+}
