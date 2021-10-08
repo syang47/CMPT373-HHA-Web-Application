fe2fc1d1aee73fad5496c8787ca3357e35fdb779
Miko Liang <lianglil@sfu.ca>
2021-10-08 03:04:08 -0700

    Update vetur.config.js

--- front-end/vetur.config.js
+++ front-end/vetur.config.js
@@ -1,16 +1,7 @@
-// Reference: https://vuejs.github.io/vetur/guide/setup.html#advanced
-
 module.exports = {
-    projects: 
-      {
-        // **optional** default: `'package.json'`
-        // Where is `package.json` in the project?
-        // We use it to determine the version of vue.
-        // It is relative to root property.
-        package: './package.json',
-        // **optional**
-        // Where is TypeScript config file in the project?
-        // It is relative to root property.
-        tsconfig: './tsconfig.json',
-      }
-  }
+    settings: {
+        "vetur.useWorkspaceDependencies": true,
+        "vetur.experimental.templateInterpolationService": true
+    },
+    default: `[{ root: './' }]`
+}
