a9f0e7d09cb22ba7c62b9cbed4e30f5077082911
Miko Liang <lianglil@sfu.ca>
2021-10-09 03:30:03 -0700

    Build the NICU/PAED input data page in front-end

---
+++ front-end/src/models/MSPPRequirement.ts
@@ -0,0 +1,15 @@
+export class MSPPRequirement {
+    bedsAvailable = null;
+    bedDays = null;
+    patientDays = null;
+    hospitalized = null;
+    dischargedAlive = null;
+    diedBefore48h = null;
+    diedAfter48h = null;
+    daysHospitalised = null;
+    referrals = null;
+    transfers = null;
+    selfDischarged = null;
+    stayedInTheWard = null;
+    admissions = null;
+}
