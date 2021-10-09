7c1d5ec6a66113c270b05b0481206a45ee02d593
Miko Liang <lianglil@sfu.ca>
2021-10-09 00:59:54 -0700

    Fixed warnings

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,56 +1,58 @@
 <template>
-   <div class="container-fluid sticky-top "> 
-    
-    <nav class="navbar navbar-expand-md navbar-light bg-light">
-      <router-link to="/home" class="navbar-brand active">
-        <img src="@/assets/logo.png" width="140" alt=""/>
-      </router-link>
-      <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
-        <span class="navbar-toggler-icon" />
-      </button>
+  <div>
+    <div class="container-fluid sticky-top "> 
+      
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+        <router-link to="/home" class="navbar-brand active">
+          <img src="@/assets/logo.png" width="140" alt=""/>
+        </router-link>
+        <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
+          <span class="navbar-toggler-icon" />
+        </button>
 
-      <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <router-link to="/department" class="nav-link">Department</router-link>
-            <router-link to="/nicu_paed" class="nav-link">NICU / PAED</router-link>
-            <router-link to="/maternity" class="nav-link">Maternity</router-link>
-            <router-link to="/rehab" class="nav-link">Rehab</router-link>
-            <router-link to="/communityhealth" class="nav-link">Community Health</router-link>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled"  href="#">Leaders Board</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#">Case Study</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#">Data Input</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" @click.prevent="logout">Log Out</a>
-          </li>
-        </ul>
-        
-        <form class="col-12 col-lg-auto mb-2 mb-lg-0 me-lg-auto">
-          <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
-        </form>
-        <div class="text-end">
-          <button type="button" class="btn btn-dark me-2">
-            <router-link to="/login" class="nav-link active text-light bg-dark">Login</router-link>
-            
-          </button>
-          <button type="button" class="btn btn-light">
-            <router-link to="/register" class="nav-link active bg-light text-dark">Register</router-link>
-          </button>
+        <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <router-link to="/department" class="nav-link">Department</router-link>
+              <router-link to="/nicu_paed" class="nav-link">NICU / PAED</router-link>
+              <router-link to="/maternity" class="nav-link">Maternity</router-link>
+              <router-link to="/rehab" class="nav-link">Rehab</router-link>
+              <router-link to="/communityhealth" class="nav-link">Community Health</router-link>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled"  href="#">Leaders Board</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">Case Study</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">Data Input</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" @click.prevent="logout">Log Out</a>
+            </li>
+          </ul>
+          
+          <form class="col-12 col-lg-auto mb-2 mb-lg-0 me-lg-auto">
+            <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+          </form>
+          <div class="text-end">
+            <button type="button" class="btn btn-dark me-2">
+              <router-link to="/login" class="nav-link active text-light bg-dark">Login</router-link>
+              
+            </button>
+            <button type="button" class="btn btn-light">
+              <router-link to="/register" class="nav-link active bg-light text-dark">Register</router-link>
+            </button>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
+    <router-view />
   </div>
-  <router-view />
 </template>
 
-<script>
+<script lang="ts">
 export default {
   
   computed: {
--- front-end/src/shims-vue.d.ts
+++ front-end/src/shims-vue.d.ts
@@ -1,13 +1,39 @@
 import VueRouter, { Route } from 'vue-router'
+import { ComponentCustomProperties } from 'vue'
+import { Store } from '@/store';// path to store file
+import Vue, {VNode} from "vue"
+
+declare global {
+    namespace JSX {
+        interface Element extends VNode {}
+        interface ElementClass extends Vue {}
+        interface IntrinsicElements {
+            [elem: string]: any
+        }
+    }
+}
+
 /* eslint-disable */
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+declare module "*.vue"{
+  import { ComponentOptions} from "vue";
+  const Component: ReturnType<typeof defineComponent>;
+  export default Component;
 }
 
 declare module 'vue/types/vue' {
   interface Vue {
     $router: VueRouter
+  }
+}
+
+declare module '@vue/runtime-core' {
+  // declare your own store states
+  interface State {
+    count: number
+  }
+
+  // provide typings for `this.$store`
+  interface ComponentCustomProperties {
+    $store: Store
   }
 }
