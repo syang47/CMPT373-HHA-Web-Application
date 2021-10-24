a932dfde405b0bab41aedf7b46ae796200c74366
zyjiang <jack_jiang_3@sfu.ca>
2021-10-23 22:03:14 -0700

    configured backend for leaders board page

---
+++ back-end/src/main/java/hha/website/DepartmentRepository.java
@@ -0,0 +1,11 @@
+package hha.website;
+
+import hha.website.models.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Integer> {
+    Department findByName(String name);
+
+}
